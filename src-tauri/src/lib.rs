4815f28774b768bf4fa4502c44d5edd921a3ee3b
Jackolix <immi.koelbl@gmail.com>
2026-02-04 10:47:23 +0100

    Add tauri-plugin-http for CORS-free API requests

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -41,6 +41,7 @@ pub fn run() {
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_http::init())
         .invoke_handler(tauri::generate_handler![greet, open_ticket_window])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
